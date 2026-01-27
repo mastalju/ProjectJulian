442d76d9bd4f30013248307efd611e3c09fcc235
mastalju <mastalju@sheridancollege.ca>
2026-01-27 16:23:17 -0500

    initial commit

---
+++ src/main/java/ice4/projectjulian/ProjectJulian.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ice4.projectjulian;
+
+/**
+ *
+ * @author julian mastalerz
+ */
+public class ProjectJulian {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
